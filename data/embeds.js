8133eb2f07971e6f39b28d59f030724dd0f26a5f
cj <endsley.co@gmail.com>
2025-06-16 01:32:34 -0600

    Add jactur-matrix iframe embed

--- data/embeds.js
+++ data/embeds.js
@@ -2,6 +2,13 @@
 // You can add either full <iframe> snippets or direct Space URLs.
 // If you provide a Space URL without '/embed', '/embed' will be appended automatically.
 const embeds = [
+  // Add your iframe snippets or embed URLs below:
+  `<iframe
+    src="https://jactur-matrix-1.static.hf.space"
+    frameborder="0"
+    width="850"
+    height="450"
+  ></iframe>`,
   // Examples:
   // 'https://huggingface.co/spaces/jactur/YourSpace/embed',
   // '<iframe src="https://huggingface.co/spaces/jactur/YourSpace/embed" allow="accelerometer; gyroscope; autoplay; encrypted-media; clipboard-write; web-share; fullscreen; microphone; camera"></iframe>',
